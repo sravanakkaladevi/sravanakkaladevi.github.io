e0c9329601c43785ceb9942fb87b6f961477f639
A SRAVAN KUMAR <sravansravan824@gmail.com>
2025-07-06 14:08:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // ✅ Required for GitHub Pages if repo is username.github.io
+  base: '/', // ✅ Required for GitHub Pages (user site)
   plugins: [react()],
 })
