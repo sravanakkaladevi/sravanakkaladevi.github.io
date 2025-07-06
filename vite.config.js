37bc2815fcef244cff8e7cfc4e87b69ed414d777
Sravan Kumar <sravansravan824@gmail.com>
2025-07-06 13:24:36 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // ✅ Required for GitHub Pages if repo is username.github.io
   plugins: [react()],
 })
